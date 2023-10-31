0a1f4e9075c074860cbcfc905ec8cd286345b989
SvenSven <mikorecare@gmail.com>
2023-10-31 22:36:44 +0800

    FEAT: Home component

--- quiz-admin/src/app/components/pages/home/home.component.ts
+++ quiz-admin/src/app/components/pages/home/home.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { Global } from "../../../modules/global";
+import { ApiService } from "../../../services/api.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
+  userBusinesses: any[] = [];
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    public global: Global,
+    private api: ApiService
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    
   }
 
+  logout() {
+    this.router.navigate([this.api.logout()]);
+  }
 }
