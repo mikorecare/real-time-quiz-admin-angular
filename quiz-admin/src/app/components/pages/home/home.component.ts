b720cf9fb89c115a161ae2cb88e2ff321558b59a
SvenSven <mikorecare@gmail.com>
2023-10-31 22:42:31 +0800

    FEAT: Install bootstrap

--- quiz-admin/src/app/components/pages/home/home.component.ts
+++ quiz-admin/src/app/components/pages/home/home.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { Router } from "@angular/router";
-import { Global } from "../../../modules/global";
-import { ApiService } from "../../../services/api.service";
+
 
 @Component({
   selector: "app-home",
@@ -10,19 +7,19 @@ import { ApiService } from "../../../services/api.service";
   styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
+  currentBusiness ={
+    name:"Current Business"
+  }
   userBusinesses: any[] = [];
   constructor(
-    private http: HttpClient,
-    private router: Router,
-    public global: Global,
-    private api: ApiService
+
   ) {}
 
   ngOnInit() {
-    
+
   }
 
   logout() {
-    this.router.navigate([this.api.logout()]);
+
   }
 }
